3970af571d81db1babf7a92e60f5be05c5c17cd2
SVBoyadzhieva18 <SVBoyadzhieva18@codingburgas.bg>
2021-07-13 11:56:16 +0300

    Moved to form2

--- ExtractColors/Form2.Designer.cs
+++ ExtractColors/Form2.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -50,48 +49,37 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(631, 178);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 29);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "label4";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(414, 178);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(428, 263);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(211, 29);
+            this.label3.Size = new System.Drawing.Size(108, 39);
             this.label3.TabIndex = 10;
-            this.label3.Text = "Contrasting color:";
+            this.label3.Text = "Color:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(631, 137);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(428, 201);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 29);
+            this.label2.Size = new System.Drawing.Size(108, 39);
             this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.label2.Text = "Color:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(481, 137);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(428, 137);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 29);
+            this.label1.Size = new System.Drawing.Size(117, 39);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Main color: ";
+            this.label1.Text = "Color: ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
@@ -115,7 +103,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -132,7 +119,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
--- ExtractColors/Form2.cs
+++ ExtractColors/Form2.cs
@@ -16,6 +16,128 @@ namespace ExtractColors
         {
             InitializeComponent();
             pictureBox1.Image = pic1;
+
+
+
+            Bitmap img = new Bitmap(pictureBox1.Image);
+
+
+
+            int width = img.Width;
+            int height = img.Height;
+
+
+
+            int br = 0;
+            Color p;
+
+
+
+            int[] rOfThePixel = new int[width * height + 1];
+            int[] gOfThePixel = new int[width * height + 1];
+            int[] bOfThePixel = new int[width * height + 1];
+
+
+
+
+            int[] sortArrayForROfThePixel = new int[width * height + 1];
+            int[] sortArrayForGOfThePixel = new int[width * height + 1];
+            int[] sortArrayForBOfThePixel = new int[width * height + 1];
+
+
+
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    p = img.GetPixel(i, j);
+                    if (p.R < 250 && p.G < 250 && p.B < 250)
+                    {
+                        sortArrayForROfThePixel[br] = p.R;
+                        sortArrayForGOfThePixel[br] = p.G;
+                        sortArrayForBOfThePixel[br] = p.B;
+
+
+
+                        rOfThePixel[br] = p.R;
+                        gOfThePixel[br] = p.G;
+                        bOfThePixel[br] = p.B;
+                    }
+
+
+
+                    br++;
+                }
+            }
+
+
+            Array.Sort(sortArrayForROfThePixel);
+            Array.Sort(sortArrayForGOfThePixel);
+            Array.Sort(sortArrayForBOfThePixel);
+
+
+            int lastRedElement = sortArrayForROfThePixel[br];
+            int lastGreenElement = sortArrayForGOfThePixel[br];
+            int lastBlueElement = sortArrayForBOfThePixel[br];
+
+            for (int i = 0; i < br; i++)
+            {
+                if (lastRedElement == lastGreenElement)
+                {
+                    lastRedElement = sortArrayForROfThePixel[br - 1];
+                }
+
+                if (lastBlueElement == lastGreenElement)
+                {
+                    lastGreenElement = sortArrayForGOfThePixel[br - 1];
+                }
+
+                if (lastRedElement == lastBlueElement)
+                {
+                    lastBlueElement = sortArrayForBOfThePixel[br - 1];
+                }
+            }
+
+
+
+            var redColor = 0;
+            var greenColor = 0;
+            var blueColor = 0;
+
+
+            for (int i = 0; i < br; i++)
+            {
+                if (lastRedElement == rOfThePixel[i])
+                {
+                    redColor = rOfThePixel[i];
+                    greenColor = gOfThePixel[i];
+                    blueColor = bOfThePixel[i];
+                    break;
+                }
+            }
+
+            for (int i = 0; i < br; i++)
+            {
+                if (lastGreenElement == gOfThePixel[i])
+                {
+                    redColor = rOfThePixel[i];
+                    greenColor = gOfThePixel[i];
+                    blueColor = bOfThePixel[i];
+                    break;
+                }
+            }
+
+            for (int i = 0; i < br; i++)
+            {
+                if (lastBlueElement == bOfThePixel[i])
+                {
+                    redColor = rOfThePixel[i];
+                    greenColor = gOfThePixel[i];
+                    blueColor = bOfThePixel[i];
+                    break;
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
